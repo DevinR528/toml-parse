1fa2639ccf85109ffb291ec20d6ae573c52c31d5
Devin R <devin.ragotzy@gmail.com>
2020-02-11 07:27:36 -0500

    removed old structured folder

--- src/structure/parse.rs
+++
@@ -1,9 +0,0 @@
-use super::err::ParseTomlError;
-use super::munch::Muncher;
-pub trait Parse {
-    type Item;
-    /// Parse a valid toml str into to a toml token.
-    ///
-    /// Item is the type returned and T is the input
-    fn parse(muncher: &mut Muncher) -> Result<Self::Item, ParseTomlError>;
-}
