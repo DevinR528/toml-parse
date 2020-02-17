a87ca4aa7d443ca24714eeed4c40d66449ac7bf0
Devin R <devin.ragotzy@gmail.com>
2020-02-17 09:42:03 -0500

    formatting

---
+++ src/toml_fmt/mod.rs
@@ -0,0 +1,15 @@
+use super::tkn_tree::{
+    walk::{
+        next_siblings, prev_non_whitespace_sibling, prev_siblings, walk_nodes, walk_non_whitespace,
+        walk_tokens, walk,
+    },
+    SyntaxNodeExtTrait, SyntaxElement, SyntaxNode, SyntaxToken, TomlKind,
+};
+
+pub struct Formatter {
+
+}
+
+impl Formatter {
+    
+}
