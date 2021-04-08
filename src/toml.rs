73e60f8bf8ef0977281c5f526c1a7e6cc02a89d4
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-04-08 16:27:45 -0400

    Before removing the ability to sort segmented tables

--- src/toml.rs
+++ src/toml.rs
@@ -1,20 +1,42 @@
-use toml_edit::{Array, Document, Item, Iter, Table, Value};
+use toml_edit::{Array, Document, Item, Table, Value};
 
 use crate::Matcher;
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Heading {
     Start(String),
+    Next(Vec<String>),
+    Complete(Vec<String>),
 }
 
-fn sort_inner(table: &mut Table, keys: &mut Vec<String>) {
+fn sort_inner(table: &mut Table, keys: &mut Vec<Heading>) {
     for (head, item) in table.iter_mut() {
         match item {
-            Item::Value(_) => break,
+            Item::Value(_) => {
+                let next = match keys.pop().unwrap() {
+                    Heading::Start(s) => Heading::Complete(vec![s]),
+                    Heading::Next(segs) => Heading::Complete(segs),
+                    // This should not be possible
+                    complete => complete,
+                };
+                keys.push(next);
+                continue;
+            }
             Item::Table(table) => {
-                if head == "workspace" {
-                    panic!("{:?}", table);
-                }
-                keys.push(head.to_string());
+                let next = match keys.pop().unwrap() {
+                    Heading::Start(s) => Heading::Next(vec![s, head.to_string()]),
+                    Heading::Next(mut segs) => {
+                        segs.push(head.to_string());
+                        Heading::Next(segs)
+                    }
+                    // This should not be possible
+                    Heading::Complete(segs) => {
+                        let next = vec![segs[0].clone(), head.into()];
+                        keys.push(Heading::Complete(segs));
+                        Heading::Next(next)
+                    }
+                };
+                keys.push(next);
                 sort_inner(table, keys)
             }
             Item::ArrayOfTables(arr) => todo!("ArrayOfTables: {:?}", arr),
@@ -26,9 +48,10 @@ fn sort_inner(table: &mut Table, keys: &mut Vec<String>) {
 }
 
 fn sort_arr(arr: &mut Array) {
+    println!("{:?}", arr);
     let mut sortable = vec![];
-    for idx in 0..arr.len() {
-        sortable.push(arr.remove(idx));
+    for _ in 0..arr.iter().count() {
+        sortable.push(arr.remove(0));
     }
     sortable.sort_unstable_by_key(|a| a.to_string());
     for item in sortable {
@@ -42,9 +65,15 @@ pub fn is_sort_toml(input: &str, matcher: Matcher) -> Document {
 
     // This takes care of `[workspace] members = [...]`
     for (heading, key) in matcher.heading_key {
-        if let Item::Table(table) = &mut toml[&heading] {
-            if let Item::Value(Value::Array(arr)) = &mut table[key] {
-                sort_arr(arr)
+        // Since this `&mut toml[&heading]` is like `SomeMap.entry(key).or_insert(Item::None)`
+        // we only want to do it if we know the heading is there already
+        if toml.as_table().contains_key(heading) {
+            if let Item::Table(table) = &mut toml[heading] {
+                if table.contains_key(key) {
+                    if let Item::Value(Value::Array(arr)) = &mut table[key] {
+                        sort_arr(arr)
+                    }
+                }
             }
         }
     }
@@ -56,14 +85,38 @@ pub fn is_sort_toml(input: &str, matcher: Matcher) -> Document {
         }
         match item {
             Item::Table(table) => {
-                headings.push(head.to_string());
-                sort_inner(table, &mut headings)
+                headings.push(Heading::Start(head.to_string()));
+                sort_inner(table, &mut headings);
             }
             Item::None => continue,
             _ => unreachable!("Top level toml must be tables"),
         }
     }
-    panic!("{:?}", headings);
+
+    headings.sort();
+
+    for (idx, heading) in headings.into_iter().enumerate() {
+        if let Heading::Complete(segs) = heading {
+            let mut table = &mut Item::None;
+
+            for seg in &segs {
+                table = if let Item::None = table {
+                    &mut toml[seg]
+                } else {
+                    &mut table[seg]
+                };
+            }
+            match &mut table {
+                Item::Table(tab) => {
+                    // tab.set_position(idx);
+                    // count += 1;
+                }
+                Item::ArrayOfTables(arr) => todo!("{:?}", arr),
+                Item::Value(_) => unreachable!("Nope"),
+                Item::None => continue,
+            }
+        }
+    }
 
     toml
 }
@@ -87,8 +140,8 @@ mod test {
 
     #[test]
     fn toml_edit_check() {
-        let input = read_to_string("examp/seg_sort.toml").expect("file read failed");
+        let input = read_to_string("examp/ftop.toml").expect("file read failed");
         let parsed = super::is_sort_toml(&input, MATCHER);
-        println!("{}", parsed.to_string())
+        println!("{}", parsed.to_string_in_original_order())
     }
 }
